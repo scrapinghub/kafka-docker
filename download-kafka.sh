f87f5c640fb9c48b26890843e6fb2510857d66c7
Shuai Lin <linshuai2012@gmail.com>
2017-04-06 07:36:46 +0800

    Fixed log-cleaner BufferOverflowException

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+# mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+# url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+url=https://dl.bintray.com/lins05/generic/kafka/kafka_2.12-0.10.2.0.tgz
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
